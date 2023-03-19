214a0f2057f47c9d2ccec4681e2bc955d1b4f763
Nathalie-Almstrom <almstromnathalie@gmail.com>
2023-03-19 21:00:27 +0100

    3 files modified, 2 added

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import SearchBar from './komponents/SearchBar.jsx';
 
 export default function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <h1>Book Store Interface</h1>
       <p>Start by searching to see some magic happen :)</p>
-      <SearchBar/>
+      <SearchBar />
     </div>
   );
 }
--- komponents/SearchBar.jsx
+++ komponents/SearchBar.jsx
@@ -1,42 +1,77 @@
 import React, { useState } from 'react';
 import SubjectSelection from '../komponents/SubjectSelection.jsx';
 import ResultCard from '../komponents/ResultCard.jsx';
+import fetchData from '../utils/fetchData.js';
+
 
 const SearchBar = () => {
   const [input, setInput] = useState(''); // used to pick up text in searchBar
-  const emptyArray = []; //this hides search results when SearchBar is not used.
-  const filteredFilms = input
-  ? props.films.filter(
-      (item) =>
-        item.title.toLowerCase().includes(search) ||
-        item.director.toLowerCase().includes(search) ||
-        item.opening_crawl.toLowerCase().includes(search)
-    )
-  : emptyArray; 
+  const [books, setBooks] = useState([]); // used to contain searchressults from API
 
-  console.log(filteredFilms)
+  const searchKey = 'Title';
+  const placeholderText = 'Search on ' + searchKey + '...';
+
+  const titleAPI = fetchData(`https://openlibrary.org/search.json?title=${input}`);
+
+    //This switch satment determines the API search: title, author, subject
+    function clickSearch () {
+      switch (searchKey) {
+        case 'Title': {
+            const data = titleAPI.read();
+          break;
+        }
+        case 'Author': {
+            console.log('this works')
+          break;
+        }
+        case 'Subject': {
+            console.log('this works')
+          break;
+        }
+      }
+    };
 
   return (
     <>
-      <div className='search-bar'>
-        <SubjectSelection/>
+      <div className="search-bar">
+        <SubjectSelection />
         <input
           className="imput-box"
           type="search"
           id="search"
-          placeholder={}
+          placeholder={placeholderText}
           value={input}
           onChange={(e) => setInput(e.target.value)}
         ></input>
-        <button className='search-butten'>
+        <button onClick={clickSearch} className="search-butten">
           Search
         </button>
-      </div> 
+      </div>
       <div className="card-container">
-        <ResultCard/>
+        <ResultCard />
       </div>
     </>
   );
 };
 
 export default SearchBar;
+
+ /*
+  //3x function to fetch searched data.
+  async function titleSearch(input) {
+    await fetch(`https://openlibrary.org/search.json?title=${input}`)
+      .then((response) => response.json())
+      .then((data) => setBooks(data.docs));
+  };
+  async function authorSearch(input) {
+    await fetch(`https://openlibrary.org/search.json?author=$${input}`)
+      .then((response) => response.json())
+      .then((data) => setBooks(data.docs));
+  };
+  async function subjectSearch(input) {
+    await fetch(`https://openlibrary.org/search.json?subject=$${input}`)
+      .then((response) => response.json())
+      .then((data) => setBooks(data.docs));
+  };
+  
+  */
